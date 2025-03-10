4b432a8f70629a8e2cab2c7cf1e30259f5d739f9
userA <userA@Email.com>
2025-03-10 16:04:59 +0800

    1

---
+++ include/ml_profiler/Runtime/Profiler.h
@@ -0,0 +1,103 @@
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllExtensions.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/ToolOutputFile.h"
+
+
+#include "TimeManager.h"
+#include <cstdint>
+#include <dlfcn.h> // for dlopen, dlsym, dlclose
+#include <filesystem>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <thread>
+
+
+class Profiler {
+
+public:
+  Profiler(std::filesystem::path mlirFilePath) : mlirFilePath(mlirFilePath) {
+    /* init MLIRContext */
+    mlir::DialectRegistry registry;
+    // Register all MLIR core dialects.
+    mlir::registerAllDialects(registry);
+    mlir::registerAllExtensions(registry);
+
+    // registry.insert<bud::BudDialect,
+    //                 trace::TraceDialect,
+    //                 dip::DIPDialect,
+    //                 dap::DAPDialect,
+    //                 rvv::RVVDialect,
+    //                 vector_exp::VectorExpDialect,
+    //                 gemmini::GemminiDialect,
+    //                 sche::ScheDialect>();
+
+    context.appendDialectRegistry(registry);
+
+    /* init TimeManager */
+    if (!timeManager) {
+      timeManager = std::make_unique<TimeManager>();
+    }
+  }
+
+  ~Profiler() = default;
+
+  std::string instrument(const std::string &targetDialect);
+
+  void compile(const std::string &target);
+
+  template <typename Func>
+  Func loadLib(const std::string &libpath, const std::string &funcName) {
+
+    //Invoke the dynamic link library
+    void *handle =
+        dlopen(libpath.c_str(), RTLD_LAZY | RTLD_GLOBAL); // invoke .so lib
+    if (!handle) {
+      std::cerr << "Failed to load shared library: " << dlerror() << std::endl;
+      return 0;
+    }
+
+    Func fptr = reinterpret_cast<Func>(dlsym(handle, funcName.c_str()));
+    char *error = dlerror();
+    if (error != NULL) {
+      std::cerr << "Failed to load symbol: " << error << std::endl;
+      dlclose(handle);
+      return nullptr;
+    }
+
+    std::cout << "Load success!" << std::endl;
+    return fptr;
+  }
+
+  void outputResult(const std::string &resultFilepath) {
+    timeManager->processTimingData(resultFilepath);
+  }
+
+  static TimeManager &getTimeManager() { return *timeManager; }
+
+private:
+  mlir::OwningOpRef<mlir::ModuleOp> parseMLIRsrcfile(std::string mlirFilePath);
+
+  void makeTarget(const std::string &target);
+
+private:
+  mlir::MLIRContext context;
+
+  std::filesystem::path mlirFilePath;
+
+  static std::unique_ptr<TimeManager> timeManager;
+};
